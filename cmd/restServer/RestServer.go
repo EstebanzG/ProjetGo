1158a0b5f4ea6d3aa167107f04f16f8ee96ff922
Corequin <cocotinbarbede@gmail.com>
2023-01-06 14:00:14 +0100

    DELETE API CALL FILE & GET DATA FROM API AND DISPLAY

--- cmd/restServer/RestServer.go
+++ cmd/restServer/RestServer.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"foo.org/myapp/internal/web/rest"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -12,6 +13,9 @@ func main() {
 	//TODO : ajouter les bornes date en paramètre
 	router.HandleFunc("/data/{sensorType}", rest.GetAllSensor)
 	//TODO : passer la date en paramètre
+	headers := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"})
+	origins := handlers.AllowedOrigins([]string{"*"})
 	router.HandleFunc("/moyenne/{date}", rest.GetMoyenneAllDataForADay)
-	http.ListenAndServe(":8080", router)
+	http.ListenAndServe(":8080", handlers.CORS(headers, methods, origins)(router))
 }
